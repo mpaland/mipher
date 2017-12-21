aeb619def01c54cb868917aa8137e988b947bda1
Marco Paland <marco@paland.com>
2017-12-21 09:28:03 +0100

    feat(index): add index file as entry

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+// \author (c) Marco Paland (marco@paland.com)
+//             2015-2017, PALANDesign Hannover, Germany
+//
+// \license The MIT License (MIT)
+//
+// This file is part of the mipher crypto library.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// \brief mipher module exports
+//
+///////////////////////////////////////////////////////////////////////////////
+
+export { Convert, Util, version } from './base';
+export { CBC, CTR, ECB } from './blockmode';
+export { AES, AES_CBC, AES_CTR, AES_CBC_PKCS7, AES_CTR_PKCS7 } from './aes';
+export { Serpent, Serpent_CBC, Serpent_CTR, Serpent_CBC_PKCS7, Serpent_CTR_PKCS7 } from './serpent';
+export { ChaCha20 } from './chacha20';
+export { Curve25519, Ed25519 } from './x25519';
+export { PBKDF2 } from './pbkdf2';
+export { HMAC, HMAC_SHA1, HMAC_SHA256, HMAC_SHA512 } from './hmac';
+export { SHA1 } from './sha1';
+export { SHA256 } from './sha256';
+export { SHA512 } from './sha512';
+export { SHA3_256, SHA3_384, SHA3_512, SHAKE128, SHAKE256 } from './sha3';
+export { UUID } from './uuid';
+export { Random } from './random';
